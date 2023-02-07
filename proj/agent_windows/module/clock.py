3dac79a888ace5f7e041d45b4071e9c9d9267fc8
Christian isnes <cisnes@outlook.com>
2023-02-07 18:30:26 +0100

    Push for cross-platform dev

---
+++ proj/agent_windows/module/clock.py
@@ -0,0 +1,10 @@
+import win32api
+class Clock():
+    def __init__(self):
+        print('asd')
+        
+    def set_clock(self, year,month,dayofweek,day,hour,minute,second,millisecond):
+        win32api.SetSystemTime(year,month,dayofweek,day,hour,minute,second,millisecond)
+   
+    def test(self):
+        print('suceess')
--- proj/clock.py
+++
@@ -1,2 +0,0 @@
-import win32api
-win32api.SetSystemTime(2021,1,0,1,0,0,0,0)
