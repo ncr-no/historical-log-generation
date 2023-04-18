0e7e495baef9c3cc9cd8b74a61f7b65a52dd5c1a
Christian Isnes <cisnes@outlook.com>
2023-04-18 17:16:27 +0200

    Timeline support for schedule and malicious events
    + some various improvements for scalability and modularity

--- src/agent_windows/scheduler.py
+++ src/agent_windows/scheduler.py
@@ -2,8 +2,9 @@ import datetime
 import json
 import calendar
 import random
-event_distribution_old = [0,0,0,0,0,2,4,8,9,10,10,10,10,9,5,3,3,2,1,0,0,0,0,0]
-event_distribution = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
+event_distribution_normal = [0,0,0,0,0,2,4,8,9,10,10,10,10,9,5,3,3,2,1,0,0,0,0,0]
+event_distribution_247 = [4,4,4,5,4,5,5,8,9,10,10,10,10,9,7,6,5,4,4,4,4,4,4,4]
+
 benign_template = json.load(open(r'E:\PRIV\HistoricalLogGen\src\agent_windows\bases\benign_template.json'))
 malicious_template = json.load(open(r'E:\PRIV\HistoricalLogGen\src\agent_windows\bases\malicious_template.json'))
 
@@ -37,36 +38,50 @@ def get_event(event_type,time):
 def save_timeline(timeline):
     #Serialize and save timeline
     json_object = json.dumps(timeline, indent=2, sort_keys=True, default=str)
-    with open('timeline.json','w') as outfile:
+    with open('file4.json','w') as outfile:
         outfile.write(json_object)
 
-def gen_timeline(start,end):
+def gen_timeline(start,end,schedule):
     start_time = datetime.datetime.strptime(start, "%d/%m/%Y")
     stop_time = datetime.datetime.strptime(end, "%d/%m/%Y")
     number_of_days= (stop_time-start_time).days
-    
     timeline = [None] * number_of_days
 
     # Generate base for timeline
+
+    # For each day generate the events
     for n in range(0,number_of_days):
         current_date = start_time.date() + datetime.timedelta(days=n)
         day = day_base.copy()
         day['date'] = current_date
         day['day_of_week'] = [current_date.weekday(),calendar.day_name[current_date.weekday()]]
-        if day['day_of_week'][0] in range(0,5):
-            # for each hour in WEEKDAYS
+        # For each day, based on 5 or 7 day schedule
+        if day['day_of_week'][0] in range(5 if schedule=='normal' else 7):
+            # Generate benign events for each hour in schedule
             for hour in range(0,23):
                 #get number of interactions for that hour
-                hourly_interactions = event_distribution_old[hour]
+                if(schedule == 'normal'):
+                    hourly_interactions = event_distribution_normal[hour]
+                else:
+                    hourly_interactions = event_distribution_247[hour]
                 
                 #for all interactions
                 for i in range (0,hourly_interactions):
                     tmp_event = get_event('benign',start_time + datetime.timedelta(days=n,hours=hour))
-                    
                     day['events'].append(tmp_event)
-                # sort the events
+                    # Sort the events
                 day['events'] = sorted(day['events'],key=lambda x: x['clock'][1])
 
+            # Generate malicious events if any
+            if(random.randint(0,100) < 10):
+                print('malicious event')
+                # Generating malicious event
+                tmp_event = get_event('malicious',start_time + datetime.timedelta(days=n,hours=random.choice([21,22,23,1,2,3,4])))
+                day['events'].append(tmp_event)
+                day['events'] = sorted(day['events'],key=lambda x: x['clock'][1])
+
+
+            # Add number of events to day
             day['number_of_events'] = len(day['events'])
         timeline[n] = day
 
@@ -76,8 +91,5 @@ def gen_timeline(start,end):
       return True
     except:
       return False
-    
-    #print(json.dumps(timeline, indent=2, sort_keys=True, default=str))
 
-
-gen_timeline('01/01/2022', '14/01/2022')
+gen_timeline('01/01/2022', '30/01/2022','normal')
